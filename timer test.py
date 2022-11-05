de765f52961ae694f5128e7528c62cfc5e6c74fc
J4yDubs <85979349+J4yDubs@users.noreply.github.com>
2022-11-05 18:55:54 -0400

    Add files via upload

--- timer test.py
+++ timer test.py
@@ -3,36 +3,20 @@ from tkinter import simpledialog
 from cmu_112_graphics import *
 import math, copy
 import time
-  
-# define the countdown func.
-# def countdown(t):
-#     t *= 60
-#     while t:
-#         mins, secs = divmod(t, 60)
-#         timer = '{:02d}:{:02d}'.format(mins, secs)
-#         # print(timer, end="\r")
-#         time.sleep(1)   # delays execution by 1s
-#         t -= 1
-      
-    # print('Tap to feed Kimchee')
-
-# def feeding():  
-  
-# input time in seconds
-# t = input("Enter the time in minutes: ")
-  
-# # function call
-# countdown(int(t))
 
 appWindow = tk.Tk() 
 ans = simpledialog.askstring("Input", "Enter desired duration",
                                 parent=appWindow)
+
+def mean(x,y):
+    return (x+y)/2
 
 def appStarted(app):
     app.cx, app.cy = app.width/2, app.height/2
     app.timer = int(ans)*60
     mins, secs = divmod(app.timer, 60)
     app.timeFormatted = '{:02d}:{:02d}'.format(mins, secs)
+    app.timerCoords = (0.9*app.cx, 1.4*app.cy, 1.1*app.cx, 1.6*app.cy)
 
 def timerFired(app):
     app.timerDelay = 1000
@@ -40,10 +24,30 @@ def timerFired(app):
     app.timeFormatted = '{:02d}:{:02d}'.format(mins, secs)
     app.timer -= 1
 
+# def keyPressed(app, event):
+
+# def mousePressed(app, event):
+#     if event.x == 
+
+def drawTimer(app, canvas):
+    fontSize = int((app.timerCoords[3]-app.timerCoords[1])//3)
+    canvas.create_rectangle(
+        app.timerCoords[0], app.timerCoords[1], 
+        app.timerCoords[2], app.timerCoords[3], 
+        fill='blue')
+    canvas.create_text(
+        app.cx, int(mean(app.timerCoords[3], app.timerCoords[1])), 
+        text=f"{app.timeFormatted}", font=f'Arial {fontSize} bold')
+
+# def drawTenMinButton(app,canvas):
+
+# def drawTwentyMinButton(app,canvas):
+
+# def drawCustTimerutton(app, canvas):
 
 def redrawAll(app, canvas):
-    fontSize = app.width//20
-    canvas.create_rectangle(app.cx-100, app.cy+50, app.cx+100, app.cy+150, fill='blue')
-    canvas.create_text(app.cx, app.cy+100, text=f"{app.timeFormatted}", font=f'Arial {fontSize} bold')
+    drawTimer(app, canvas)
 
-runApp(width=400, height=400)
+appWidth = 800
+appHeight = (appWidth//16)*9
+runApp(width=appWidth, height=appHeight)
