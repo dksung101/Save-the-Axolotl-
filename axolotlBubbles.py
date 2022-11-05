7c0310a96c85ba8cd2a9401e0b4a8d30b63a379c
Daniel Sung <csdsung101@gmail.com>
2022-11-05 17:29:01 -0400

    updated bubbles file

--- axolotlBubbles.py
+++ axolotlBubbles.py
@@ -9,31 +9,41 @@ def appStarted(app):
     app.margin = 0
     app.timerDelay = 50
     app.totalTime=0
+    app.count = 0
 
 def mousePressed(app, event):
-    pass
+    for (row, col, speed, radius) in app.bubbles:
+        (x0, y0, x1, y1) = getCellBounds(app, row, col)
+        if (event.x >= x0-radius and event.x <= x1+radius) and (event.y >= y0-radius and event.y <= y1+radius):
+            app.bubbles.remove((row, col, speed, radius))
+            app.count+=1
+            # popBubble(app, (row, col, speed))
 
 def drawBubbles(app, canvas):
-    for (row, col, speed) in app.bubbles:
+    for (row, col, speed, radius) in app.bubbles:
         (x0, y0, x1, y1) = getCellBounds(app, row, col)
-        canvas.create_oval(x0, y0, x1, y1, width=1.5)
+        canvas.create_oval(x0-radius, y0-radius, x1+radius, y1+radius, width=1.5)
+        
+# def popBubble(app):
+#     for 
     
 def moveBubbleUp(app):
     newLocations = []
     # speed = random.randint(0, 3)
 
-    for (row, col, speed) in app.bubbles:
+    for (row, col, speed, radius) in app.bubbles:
         newCol = col
         newRow = row-speed
         if (newRow < app.rows) and (newRow>=0):
-            newLocations.append((newRow, newCol, speed))
+            newLocations.append((newRow, newCol, speed, radius))
     app.bubbles = newLocations
     
 def createBubble(app):
     row = app.rows-1
     col = random.randint(0, app.cols-1)
     speed = random.randint(1, 3)*0.2
-    app.bubbles.append((row, col, speed))
+    radius = random.randint(5, 25)
+    app.bubbles.append((row, col, speed, radius))
     
 def getCellBounds(app, row, col):
     colWidth = (app.width-2*app.margin) / app.cols
@@ -50,8 +60,11 @@ def timerFired(app):
         createBubble(app)
     moveBubbleUp(app)
 
+def drawCount(app, canvas):
+    canvas.create_text(app.width/2, app.height-40, text=f'Score: {app.count}!', fill='red', font='TimesNewRoman 12 bold')
 
 def redrawAll(app, canvas):
     drawBubbles(app, canvas)
+    drawCount(app, canvas)
 
 runApp(width=400, height=800)
