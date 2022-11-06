e158646e83ae2c10810868b89b2f2b55a3e578c7
J4yDubs <85979349+J4yDubs@users.noreply.github.com>
2022-11-05 20:55:21 -0400

    Add files via upload

--- timer test.py
+++ timer test.py
@@ -4,48 +4,62 @@ from cmu_112_graphics import *
 import math, copy
 import time
 
-ans = simpledialog.askstring("Timer duration input", 
-"Enter desired timer duration")
-
 def mean(x,y):
     return (x+y)/2
 
 def appStarted(app):
+    app.timerConfigState = False
     app.cx, app.cy = app.width/2, app.height/2
-    app.timer = int(ans)*60
-    mins, secs = divmod(app.timer, 60)
-    app.timeFormatted = '{:02d}:{:02d}'.format(mins, secs)
-    app.timerCoords = (0.9*app.cx, 1.4*app.cy, 1.1*app.cx, 1.6*app.cy)
+    app.timerDuration = None
+    app.timeFormatted = None
+    app.timerCoords = (0.75*app.cx, 1.4*app.cy, 1.25*app.cx, 1.6*app.cy)
 
 def timerFired(app):
     app.timerDelay = 1000
-    mins, secs = divmod(app.timer, 60)
-    app.timeFormatted = '{:02d}:{:02d}'.format(mins, secs)
-    app.timer -= 1
+    if app.timerDuration != None:
+        mins, secs = app.timerDuration // 60, app.timerDuration % 60
+        app.timeFormatted = '{:02d}:{:02d}'.format(mins, secs)
+        app.timerDuration -= 1
 
-# def keyPressed(app, event):
-
-# def mousePressed(app, event):
-#     if event.x == 
+def mousePressed(app, event):
+    if event.x <= app.timerCoords[2] and event.x >= app.timerCoords[0] \
+        and event.y <= app.timerCoords[3] and event.y >= app.timerCoords[1]:
+        app.timerDuration = simpledialog.askstring("Timer duration input", 
+        "Enter desired timer duration")
+        if app.timerDuration != None:
+            app.timerDuration = int(app.timerDuration)*60
+            mins, secs = app.timerDuration // 60, app.timerDuration % 60
+            app.timeFormatted = '{:02d}:{:02d}'.format(mins, secs)
+            app.timerConfigState = True
 
 def drawTimer(app, canvas):
     fontSize = int((app.timerCoords[3]-app.timerCoords[1])//3)
     canvas.create_rectangle(
         app.timerCoords[0], app.timerCoords[1], 
         app.timerCoords[2], app.timerCoords[3], 
-        fill='blue')
+        fill='blue', width=0)
     canvas.create_text(
         app.cx, int(mean(app.timerCoords[3], app.timerCoords[1])), 
-        text=f"{app.timeFormatted}", font=f'Arial {fontSize} bold')
+        text=f"{app.timeFormatted}", font=f'Arial {fontSize} bold', fill='white')
 
 # def drawTenMinButton(app,canvas):
-
 # def drawTwentyMinButton(app,canvas):
 
-# def drawCustTimerutton(app, canvas):
+def drawCustTimerButton(app, canvas):
+    fontSize = int((app.timerCoords[3]-app.timerCoords[1])//3)
+    canvas.create_rectangle(
+        app.timerCoords[0], app.timerCoords[1], 
+        app.timerCoords[2], app.timerCoords[3], 
+        fill='white')
+    canvas.create_text(
+        app.cx, int(mean(app.timerCoords[3], app.timerCoords[1])), 
+        text=f"Custom duration", font=f'Arial {fontSize} bold')
 
 def redrawAll(app, canvas):
-    drawTimer(app, canvas)
+    if app.timerConfigState:
+        drawTimer(app, canvas)
+    else: 
+        drawCustTimerButton(app, canvas)
 
 appWidth = 800
 appHeight = (appWidth//16)*9
