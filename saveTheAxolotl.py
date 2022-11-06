5b362518e46ae13fb3f9a9e9e603150ca4846e07
J4yDubs <85979349+J4yDubs@users.noreply.github.com>
2022-11-05 23:11:26 -0400

    Add files via upload

--- saveTheAxolotl.py
+++ saveTheAxolotl.py
@@ -1,6 +1,14 @@
 from cmu_112_graphics import *
-import tkinter
+import tkinter as tk
+from tkinter import simpledialog 
 import random
+from cmu_112_graphics import *
+import math, copy
+import time
+
+# helper function
+def mean(x,y):
+    return (x+y)/2
 
 def appStarted(app):
     app.bubbles = []
@@ -12,8 +20,7 @@ def appStarted(app):
     app.totalTime=0
     app.count = 0
     #axolotl face
-    app.cx = app.width/2
-    app.cy = app.height/2
+    app.cx, app.cy = app.width/2, app.height/2
     app.rw = 150 
     app.rh = 90
     #lives 
@@ -21,9 +28,13 @@ def appStarted(app):
     app.heartR = 5
     app.minOil = 5
     app.threshold = 10
-    app.state = True
     app.mood = 'happy'
-    # app.foodCollected = 20
+    # timer
+    # app.timeInSeconds = None    # this is the secondary 30s timer
+    app.timerConfigState = None
+    app.timerDuration = None
+    app.timeFormatted = None
+    app.timerCoords = (0.75*app.cx, 1.4*app.cy, 1.25*app.cx, 1.6*app.cy)
 
 def mousePressed(app, event):
     for (row, col, speed, radius) in app.bubbles:
@@ -33,10 +44,20 @@ def mousePressed(app, event):
             app.count+=1
             # popBubble(app, (row, col, speed))
 
+    if app.timerConfigState == None and event.x <= app.timerCoords[2] and event.x >= app.timerCoords[0] \
+        and event.y <= app.timerCoords[3] and event.y >= app.timerCoords[1]:
+        app.timerDuration = simpledialog.askstring("Timer duration input", 
+        "Enter desired timer duration")
+        if app.timerDuration != None:
+            app.timerDuration = int(app.timerDuration)*60
+            mins, secs = app.timerDuration // 60, app.timerDuration % 60
+            app.timeFormatted = '{:02d}:{:02d}'.format(mins, secs)
+            app.timerConfigState = True
+
 def drawBubbles(app, canvas):
     for (row, col, speed, radius) in app.bubbles:
         (x0, y0, x1, y1) = getCellBounds(app, row, col)
-        canvas.create_oval(x0-radius, y0-radius, x1+radius, y1+radius, outline = 'black', width=1.5, fill="black")
+        canvas.create_oval(x0-radius, y0-radius, x1+radius, y1+radius, fill='black')
         
     
 def moveBubbleUp(app):
@@ -67,11 +88,26 @@ def getCellBounds(app, row, col):
     return (x0, y0, x1, y1)
 
 def timerFired(app):
-    app.totalTime+=app.timerDelay
-    print(app.totalTime)
-    if app.totalTime%500 == 0 and app.totalTime<=30000:
-        createBubble(app) 
-    moveBubbleUp(app)
+    print(f"app.totalTime:{app.totalTime}")
+    if not app.timerConfigState:
+        app.totalTime+=app.timerDelay
+        if app.timerConfigState == False and app.totalTime%500 == 0 and app.totalTime<=30000:
+            createBubble(app) 
+        moveBubbleUp(app)
+        if app.totalTime > 30000:
+            app.totalTime = 0
+    if app.totalTime%1 == 0 and app.timerDuration != None:  # change delay here
+        mins, secs = app.timerDuration // 60, app.timerDuration % 60
+        app.timeFormatted = '{:02d}:{:02d}'.format(mins, secs)
+        app.timerDuration -= 1
+    
+    if app.timerDuration == 0: 
+        app.timerConfigState = False
+        app.totalTime = 0
+        app.timerDuration = None
+
+    if app.totalTime == 30000 and app.timerConfigState == False: 
+        app.timerConfigState = None
 
     if len(app.lives) == 3:
         app.mood = 'happy'
@@ -80,7 +116,7 @@ def timerFired(app):
     else:
         app.mood = 'frown'
 
-    if app.totalTime%30000 == 0:
+    if app.totalTime == 29999 and app.timerConfigState == False:
         changeLives(app, app.count)
         app.count = 0
 
@@ -89,14 +125,19 @@ def checkMood(app, canvas):
         app.mood = 'happy'
     elif len(app.lives) == 2:
         app.mood = 'neutral'
-    else:
+    elif len(app.lives) == 1:
         app.mood = 'frown'
+    elif app.timerConfigState==True:
+        app.mood = 'sleep'
 
 def drawTimer(app, canvas):
     timeInSeconds = 30-app.totalTime//1000
     if timeInSeconds<0:
         timeInSeconds = 0
-    canvas.create_text(app.width/2, app.height-640, text=f'Time LEFT: {timeInSeconds}!', fill='red', font='TimesNewRoman 12 bold')
+    if timeInSeconds > 10:
+        canvas.create_text(app.width/2, app.height-640, text=f'{timeInSeconds} seconds left', fill='black', font='TimesNewRoman 12 bold')
+    else: 
+        canvas.create_text(app.width/2, app.height-640, text=f'Only {timeInSeconds} seconds left!', fill='red', font='TimesNewRoman 12 bold')
 
 def drawCount(app, canvas):
     canvas.create_text(app.width/2, app.height-40, text=f'Score: {app.count}!', fill='red', font='TimesNewRoman 12 bold')
@@ -157,6 +198,7 @@ def drawAxolotl(app, canvas, cx, cy, rw, rh):
 def changeLives(app, foodCollected):
     if foodCollected < app.minOil:
         app.lives.pop(0)
+        print(app.lives)
     elif foodCollected > app.threshold and len(app.lives) < 3:
         life = app.lives[0] 
         app.lives.insert(0,(life[0] - 40, 30))
@@ -174,12 +216,36 @@ def drawLives(app, canvas):
                               cx + app.heartR*2, cy,
                               cx, cy*1.25, fill = "red")
 
+def drawMainTimer(app, canvas):
+    fontSize = int((app.timerCoords[3]-app.timerCoords[1])//3)
+    canvas.create_rectangle(
+        app.timerCoords[0], app.timerCoords[1], 
+        app.timerCoords[2], app.timerCoords[3], 
+        fill='blue', width=0)
+    canvas.create_text(
+        app.cx, int(mean(app.timerCoords[3], app.timerCoords[1])), 
+        text=f"{app.timeFormatted}", font=f'Arial {fontSize} bold', fill='white')
+    
+def drawCustTimerButton(app, canvas):
+    fontSize = int((app.timerCoords[3]-app.timerCoords[1])//3)
+    canvas.create_rectangle(
+        app.timerCoords[0], app.timerCoords[1], 
+        app.timerCoords[2], app.timerCoords[3], 
+        fill='white')
+    canvas.create_text(
+        app.cx, int(mean(app.timerCoords[3], app.timerCoords[1])), 
+        text=f"Custom duration", font=f'Arial {fontSize} bold')
+
 def redrawAll(app, canvas):
     drawAxolotl(app, canvas, app.cx, app.cy, app.rw, app.rh)
     drawLives(app, canvas)
-    drawBubbles(app, canvas)
     drawCount(app, canvas)
-    drawTimer(app, canvas)
-    # changeLives(app, canvas, app.count)
+    if app.timerConfigState == False:
+        drawTimer(app, canvas)
+        drawBubbles(app, canvas)
+    elif app.timerConfigState == True:
+        drawMainTimer(app, canvas)
+    elif app.timerConfigState == None:
+        drawCustTimerButton(app, canvas)
 
 runApp(width=440, height = 680)
